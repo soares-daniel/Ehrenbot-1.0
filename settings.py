45236cdbcffc1500c7f20623eca78e3c84b43c8c
soares-daniel <daniel.soares.001@student.uni.lu>
2023-02-18 02:25:12 +0100

    no prod message

--- settings.py
+++ settings.py
@@ -5,14 +5,13 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 DEBUG = os.getenv('DEBUG')
-print("------")
 if DEBUG:
+    print("------")
     print("DEBUG mode is enabled")
     env_path = Path(".") / ".env.debug"
     load_dotenv(dotenv_path=env_path)
     from settings_files.development import *
 else:
-    print("DEBUG mode is disabled, we are in production.")
     env_path = Path(".") / ".env"
     load_dotenv(dotenv_path=env_path)
     from settings_files.production import *
