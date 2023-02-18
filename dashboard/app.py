369cd8b094810c235dda6ba5309ebcd794ed236f
soares-daniel <daniel.soares.001@student.uni.lu>
2023-02-18 02:11:39 +0100

    logging for webapp

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,8 +1,27 @@
+import logging
+
+from logging import handlers
 from flask import Flask, render_template, request, redirect, url_for
 from settings import DISCORD_BOT_TOKEN
 
+# Initialize Flask app
 app = Flask(__name__, template_folder='templates')
 app.secret_key = DISCORD_BOT_TOKEN
+
+# Logging
+app.logger.setLevel(logging.WARNING)
+file_handler = handlers.TimedRotatingFileHandler(
+    filename="logs/webapp.log",
+    when="midnight",
+    backupCount=7
+)
+formatter = logging.Formatter('%(asctime)s - %(levelname)-8s: %(message)s [in %(pathname)s:%(lineno)d]')
+file_handler.setFormatter(formatter)
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.ERROR)
+app.logger.addHandler(file_handler)
+app.logger.addHandler(stream_handler)
 
 @app.route('/')
 def index():
@@ -13,10 +32,8 @@ def login():
     if request.method == 'POST':
         # Authenticate user credentials and redirect to dashboard page
         return redirect(url_for('dashboard'))
-    else:
-        return render_template('login.html')
+    return render_template('login.html')
 
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
-
