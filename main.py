337d901bc45e579765c213c26a8777dc29ce29b8
soares-daniel <daniel.soares.001@student.uni.lu>
2023-02-20 02:18:28 +0100

    remove dashboard test

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import os
-import threading
-
-from dashboard.app import app
 from ehrenbot import Ehrenbot
 from settings import DISCORD_BOT_TOKEN, SERVER_PORT
 
@@ -10,11 +7,5 @@ bot = Ehrenbot()
 for filename in os.listdir("./ehrenbot/cogs"):
     if filename.endswith(".py") and filename != "__init__.py":
         bot.load_extension(f"ehrenbot.cogs.{filename[:-3]}")
-
-def run_app():
-    app.run(host='0.0.0.0', port=SERVER_PORT, debug=False)
-
-web_thread = threading.Thread(target=run_app)
-web_thread.start()
 
 bot.run(DISCORD_BOT_TOKEN)
