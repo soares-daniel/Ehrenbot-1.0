222eab1b8771b04ff2393711576f4088d9966bdb
soares-daniel <daniel.soares.001@student.uni.lu>
2023-02-28 17:33:18 +0100

    prevent downtime issues, removed prints for status

--- ehrenbot/utils/utils_status.py
+++ ehrenbot/utils/utils_status.py
@@ -19,8 +19,6 @@ async def check_user_status(bot: Ehrenbot, destiny_membership_id: int, membershi
     response = await destiny2.GetProfile(destiny_membership_id=destiny_membership_id,
                                          membership_type=membership_type,
                                          components=[100])
-    print("GetProfile")
-    print(response)
     return update_status(response, status)
 
 async def check_api_status(bot: Ehrenbot) -> bool:
@@ -28,8 +26,6 @@ async def check_api_status(bot: Ehrenbot) -> bool:
     destiny2 = bot.destiny_client.destiny2
     status = {"Status": "**ERROR**", "Category": "API", "Used Endpoint": "`GetDestinyManifest`"}
     response = await destiny2.GetDestinyManifest()
-    print("GetDestinyManifest")
-    print(response)
     return update_status(response, status)
 
 async def check_vendor_status(bot: Ehrenbot, destiny_membership_id: int, membership_type: int, character_id: int, token: dict) -> bool:
@@ -41,8 +37,6 @@ async def check_vendor_status(bot: Ehrenbot, destiny_membership_id: int, members
                                          destiny_membership_id=destiny_membership_id,
                                          membership_type=membership_type,
                                          components=[400])
-    print("GetVendors")
-    print(response)
     return update_status(response, status)
 
 async def check_group_v2_status(bot: Ehrenbot, group_id: int) -> bool:
@@ -50,6 +44,4 @@ async def check_group_v2_status(bot: Ehrenbot, group_id: int) -> bool:
     group_v2 = bot.destiny_client.group_v2
     status = {"Status": "**ERROR**", "Category": "GroupV2", "Used Endpoint": "`GetGroup`"}
     response = await group_v2.GetGroup(group_id=group_id)
-    print("GetGroup")
-    print(response)
     return update_status(response, status)
