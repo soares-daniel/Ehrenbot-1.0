87b2b65613aef21527931e593d47c894f86ca6f4
soares-daniel <daniel.soares.001@student.uni.lu>
2023-03-03 22:45:47 +0100

    fixed kick

--- ehrenbot/cogs/clan_admin.py
+++ ehrenbot/cogs/clan_admin.py
@@ -22,14 +22,14 @@ class ClanAdmin(commands.Cog):
     @commands.has_role("Clan Admin")
     async def kick(self, ctx: discord.ApplicationContext, member: discord.Member):
         member_collection = self.bot.database["members"]
-        member = member_collection.find_one({"discord_id": member.id})
-        if not member:
+        member_info = member_collection.find_one({"discord_id": member.id})
+        if not member_info:
             await ctx.send(
-                "The member you wanted to kick was not found. Please contact an admin."
+                f"{member.name} you wanted to kick was not found. Please contact an admin."
             )
             return
-        admin = member_collection.find_one({"discord_id": ctx.author.id})
-        if not admin:
+        admin_info = member_collection.find_one({"discord_id": ctx.author.id})
+        if not admin_info:
             await ctx.send(
                 "Your Discord ID was not found in the database. Please contact an admin."
             )
@@ -41,20 +41,19 @@ class ClanAdmin(commands.Cog):
                 "Your Bungie.Net token was not found. Please contact an admin."
             )
             return
-        membership_id = member["destiny_profile"]["membership_id"]
-        membership_type = member["destiny_profile"]["membership_type"]
-        admin_group_id = admin["destiny_profile"]["group_id"]
+        membership_id = member_info["destiny_profile"]["destiny_membership_id"]
+        membership_type = member_info["destiny_profile"]["membership_type"]
+        admin_group_id = admin_info["destiny_profile"]["group_id"]
         response = await self.bot.destiny_client.group_v2.KickMember(
-            token=admin_token,
+            token=admin_token["token"],
             group_id=admin_group_id,
             membership_id=membership_id,
             membership_type=membership_type,
         )
         if response["ErrorCode"] != 1:
-            await ctx.send(
+            await ctx.respond(
                 f"An error occurred while trying to kick the member. Please contact an admin.\n"
                 f"Error code: {response['ErrorCode']}",
-                hidden=True,
                 delete_after=5,
             )
             # Notify bot_admin
@@ -65,7 +64,7 @@ class ClanAdmin(commands.Cog):
             return
         if response["ErrorCode"] == 1:
             await member.kick(reason="Kicked from clan.")
-            await ctx.send(f"Kicked {member.mention} from the clan and from the server.", hidden=True, delete_after=5)
+            await ctx.respond(f"Kicked {member.name} from the clan and from the server.", delete_after=5)
 
 def setup(bot):
     bot.add_cog(ClanAdmin(bot))
